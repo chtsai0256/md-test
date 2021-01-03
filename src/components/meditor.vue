20b2bbfeb27aa4d3b8208db47cda3d25ee560833
chtsai0256 <56692261+chtsai0256@users.noreply.github.com>
2021-01-04 00:08:01 +0800

    mdtest

---
+++ src/components/meditor.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+  <mynav/>
+  <markdown @on-save="save" ref="md" 
+             @on-change="changeImg" 
+             :theme="theme" 
+             :initialValue="initialValue" 
+             :placeholderValue="placeholderValue"
+             :height='1000'/>
+</div>
+</template>
+<script>
+import mynav from './nav.vue'
+import markdown from 'vue-meditors'
+export default {
+  name: 'meditor',
+  components:{ 
+    markdown,
+    mynav
+  },
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      placeholderValue: "請輸入問題描述",
+      initialValue: "",
+      theme: 'OneDark'
+    }
+  },
+  motheds:{
+    saveMd(){
+      console.log(this.$refs.handleSave)
+      this.$refs.md.handleSave();
+    },
+    save(content){
+      console.log("原始内容："+content.value);
+      console.log("转义后的内容："+content.html);
+      console.log("主题theme："+content.theme);
+    },
+    changeImg(files,callback){
+      console.log(files)
+      //图片上传
+      var url = "";
+      callback(url);//通过callback回调将图片地址返回
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,76 @@
+<template>
+<div>
+  <b-navbar fixed="top" toggleable="lg" type="light" variant="faded">
+    <b-navbar-brand href="#">
+      <b-icon icon="gear-fill" aria-hidden="true"></b-icon>
+      <span> HOME   </span>
+    </b-navbar-brand>
+    <b-button-group>
+      <b-button variant="light"><b-icon icon="pencil-fill" variant="dark"></b-icon></b-button>
+      <b-button variant="light"><b-icon icon="square-half" variant="dark"></b-icon></b-button>
+      <b-button variant="light"><b-icon icon="eye-fill" variant="dark"></b-icon></b-button>
+    </b-button-group>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">指令說明</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+       <b-nav-item href="#">指令說明</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'my_nav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
